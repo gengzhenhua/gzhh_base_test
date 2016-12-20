31f2586223c95f553c5a6f80109fb23c77bc7cb5
gengzhenhua <gengzhenhua@jd.com>
2016-12-20 10:15:58 +0800

    new test

---
+++ src/main/java/com/gzhh/study/integer/IntegerTest.java
@@ -0,0 +1,34 @@
+package com.gzhh.study.integer;
+
+/**
+ * @author gengzhenhua
+ *
+ * @description: Add the class description here.
+ */
+public class IntegerTest {
+    
+    public static void main(String[] args) {
+
+        Integer a = 1;
+        
+        Integer b = 2;
+        
+        Integer c = 3;
+        
+        Integer d = 3;
+        
+        Integer e = 321;
+        
+        Integer f = 321;
+        
+        Long g = 3L;
+        
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+    }
+
+}
